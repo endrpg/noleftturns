68fef325e275055949b8c0dfc61820454726833a
endrpg <michael420mabes@outlook.com>
2020-10-23 07:36:37 -0400

    Version1
    Main Menu/Credits
    Asset Packs
    Idea

---
+++ noleftturns/Assets/MainMenu/SetVolume.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class SetVolume : MonoBehaviour
+{
+    public AudioMixer mixer;
+    public Slider slider;
+
+    void Start()
+    {
+        slider.value = PlayerPrefs.GetFloat("MusicVolume", 0.75f);
+    }
+
+    public void MasterLevel()
+    {
+        float sliderValue = slider.value;
+        mixer.SetFloat("MasterVolume", Mathf.Log10(sliderValue) * 20);
+        PlayerPrefs.SetFloat("MasterVolume", sliderValue);
+    }
+    public void MusicLevel()
+    {
+        float sliderValue = slider.value;
+        mixer.SetFloat("MusicVolume", Mathf.Log10(sliderValue) * 20);
+        PlayerPrefs.SetFloat("MusicVolume", sliderValue);
+    }
+    public void SFXLevel()
+    {
+        float sliderValue = slider.value;
+        mixer.SetFloat("SFXVolume", Mathf.Log10(sliderValue) * 20);
+        PlayerPrefs.SetFloat("SFXVolume", sliderValue);
+    }
+}
